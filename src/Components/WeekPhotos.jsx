5cd24f52ef26701a2230a6cf2b1e976adb2095bb
Benjamin Garrido <benjamin.garrido@qrsof.com>
2022-03-28 16:06:55 -0600

    refactor ui

--- src/Components/WeekPhotos.jsx
+++ src/Components/WeekPhotos.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { getWeekPhotos } from "../Api/getWeekPhotos";
 import { ItemCard } from "./ItemCard";
 
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+
+import 'swiper/swiper.min.css';
+
+import "./styles.css";
+
+import { Pagination } from "swiper";
+
 
 export function WeekPhotos() {
 	
@@ -16,15 +24,32 @@ export function WeekPhotos() {
 	return (<div >
 		<h4>Fotos de la semana</h4>
 		<div className='weekPhotos'>
+			
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={30}
+				pagination={{
+					clickable: true,
+				}}
+				modules={[Pagination]}
+				className="mySwiper"
+			>
+			
 			{weekPhotos.map(
 				photo => (
+					<SwiperSlide>
+					
 					<ItemCard
 						key ={photo.date}
 						item={photo}
 					/>
+					</SwiperSlide>
+				
 				)
 			)}
+			</Swiper>
 		</div>
 		</div>);
 };
+
 
